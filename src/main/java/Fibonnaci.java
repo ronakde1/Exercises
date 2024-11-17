acb9000b4a1b125b7ab86ac2d1a95c844a17670e
Ronak De <ronakde84@gmail.com>
2024-11-17 12:13:18 +0000

    Exercise 3 branch

---
+++ src/main/java/Fibonnaci.java
@@ -0,0 +1,17 @@
+
+/** A class to compute Fibonacci numbers using a simple recursive approach. */
+class Fibonacci {
+
+    /**
+     * Compute a Fibonacci number.
+     *
+     * @param i the index in the Fibonacci sequence
+     * @return the Fibonacci number for this index
+     */
+
+    int fib(int i) {
+        if (i<0) throw new IllegalArgumentException();
+        else return 1;
+
+    }
+}
